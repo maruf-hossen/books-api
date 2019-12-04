cdf4ef18c897b5a298d7a4ec6063a2a70eb1b1aa
maruf hossen <maruf15-5445@diu.edu.bd>
2019-12-04 21:44:22 +0600

    first commit

---
+++ src/main/java/com/example/api/controller/BookController.java
@@ -0,0 +1,55 @@
+package com.example.api.controller;
+import com.example.api.exception.BookNotFoundException;
+import com.example.api.model.Book;
+import com.example.api.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import javax.validation.Valid;
+import java.util.List;
+@RestController
+public class BookController {
+	@Autowired
+		BookRepository bookRepository;
+	//Get all notes
+	@GetMapping("/books")
+	public List<Book> getAllNotes(){
+		return bookRepository.findAll();
+	}
+	
+	@PostMapping("/books")
+	public Book createNote(@Valid @RequestBody Book book) {
+		return bookRepository.save(book);
+	}
+	@GetMapping("/books/{id}")
+	public Book getNoteById(@PathVariable(value="id") Long bookId) throws BookNotFoundException{
+		return bookRepository.findById(bookId)
+				.orElseThrow(()-> new BookNotFoundException(bookId));
+	}
+	// Update a Note
+    @PutMapping("/books/{id}")
+    public Book updateNote(@PathVariable(value = "id") Long bookId,
+                           @Valid @RequestBody Book bookDetails) throws BookNotFoundException {
+
+Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException(bookId));
+
+book.setBook_name(bookDetails.getBook_name());
+        book.setAuthor_name(bookDetails.getAuthor_name());
+        book.setIsbn(bookDetails.getIsbn());
+
+Book updatedBook = bookRepository.save(book);
+
+return updatedBook;
+    }
+ // Delete a Note
+    @DeleteMapping("/books/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable(value = "id") Long bookId) throws BookNotFoundException {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new BookNotFoundException(bookId));
+
+bookRepository.delete(book);
+
+return ResponseEntity.ok().build();
+    }
+}
